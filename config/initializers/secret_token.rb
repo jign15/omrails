1d1030146265fc476373a32f878f36ccc4de2abd
Julian Garcia <jgarcia@instagis.com>
2013-08-23 11:58:44 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '6328644a646344abad1b16c1d3a93d4b7ae15b2170c537bf96681f6511ab8d2519268d617b1d55065e2283173c4f501c41f8464c017e44863667377d61cc15ff'
